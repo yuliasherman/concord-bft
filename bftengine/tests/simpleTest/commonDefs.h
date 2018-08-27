2939124e2fbc50248147ddfd45bde80a897d5909
Michal Tomaszewski <michalt48@gmail.com>
2018-08-27 08:54:56 -0700

    Add gh-pages

--- bftengine/tests/simpleTest/commonDefs.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define READ_VAL_REQ ((uint64_t)100)
-#define SET_VAL_REQ  ((uint64_t)200)
-
--- bftengine/tests/simpleTest/scripts/runClient.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-scriptdir=$(cd $(dirname $0); pwd -P)
-
-$scriptdir/../client
